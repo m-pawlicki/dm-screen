dc8faf2dd9d93b163be7056c759331c99fa9f4ac
Micah Pawlicki <mich.pawlicki@gmail.com>
2026-03-31 16:53:03 -0500

    updating models

---
+++ models/races.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Races(Enum):
+    AAS = "Aasimar"
+    DRB = "Dragonborn"
+    DWF = "Dwarf"
+    ELF = "Elf"
+    HEL = "Half-Elf"
+    GNO = "Gnome"
+    GOL = "Goliath"
+    HLF = "Halfling"
+    HUM = "Human"
+    ORC = "Orc"
+    HOR = "Half-Orc"
+    TFL = "Tiefling"
--- models/stats.py
+++ models/stats.py
@@ -1,9 +1,34 @@
 from enum import Enum
+import math, json
 
-class Stats(Enum):
+class StatNames(Enum):
     STR = "Strength"
     DEX = "Dexterity"
     CON = "Constitution"
     INT = "Intelligence"
     WIS = "Wisdom"
     CHA = "Charisma"
+
+class Stats():
+
+    def __init__(self, str, dex, con, int, wis, cha):
+        self.str = str
+        self.dex = dex
+        self.con = con
+        self.int = int
+        self.wis = wis
+        self.cha = cha
+
+    def calc_modifier(self, stat):
+        return math.floor((stat - 10)/2)
+    
+    def to_json(self):
+        obj = {
+            StatNames.STR: self.str,
+            StatNames.DEX: self.dex,
+            StatNames.CON: self.con,
+            StatNames.INT: self.int,
+            StatNames.WIS: self.wis,
+            StatNames.CHA: self.cha
+        }
+        return json.dumps(obj)
