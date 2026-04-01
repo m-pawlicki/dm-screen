6af4ec6026e543967b54284c0c9ac714df9a431d
Micah Pawlicki <mich.pawlicki@gmail.com>
2026-04-01 16:10:59 -0500

    update gitignore

--- models/character.py
+++ models/character.py
@@ -1,4 +1,7 @@
 class Character:
-    def __init__(self, char_name, player_name):
+    def __init__(self, char_name, player_name, alignment):
         self.character_name = char_name
         self.player_name = player_name
+        self.level = 0
+        self.exp = 0
+        self.alignment = alignment
